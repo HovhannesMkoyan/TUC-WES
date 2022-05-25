64c883ce347ce26cfc64e2d94ff826f55ec2230a
Hovhannes <hovhannes.mkoyan@gmail.com>
2022-05-26 00:36:23 +0400

    designing

--- react-todo/src/components/Todos/Todos.jsx
+++ react-todo/src/components/Todos/Todos.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareFull, faCheck, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+import "./Todos.css";
 
 export default function Todos() {
   return (
-    <div>Todos</div>
+    <div className="todos-container">
+      <div className="todo-container">
+        <div>
+          <FontAwesomeIcon icon={faSquareFull} size="xs" />
+          <span>Make a cup of cofee</span>
+        </div>
+        <div className="todo-actions-container">
+          <FontAwesomeIcon icon={faCheck} size="xs" />
+          <FontAwesomeIcon icon={faTrash} size="xs" />
+        </div>
+      </div>
+      <div className="todo-container">
+        <div>
+          <FontAwesomeIcon icon={faSquareFull} size="xs" />
+          <span>Make a cup of cofee</span>
+        </div>
+        <div className="todo-actions-container">
+          <FontAwesomeIcon icon={faCheck} size="xs" />
+          <FontAwesomeIcon icon={faTrash} size="xs" />
+        </div>
+      </div>
+    </div>
   )
 }
